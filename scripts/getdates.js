cea965a577724cc56dbadbe0bb8d4dca1ad961e3
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2024-04-23 09:11:46 +0100

    Completed js for week one.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,50 @@
+function getCurrentYear() {
+    return new Date().getFullYear();
+}
+
 function insertDateAndIcon() {
-    const dateNow = new Date.getFullYear();
-
-    const dateElement = document.getElementById('currentyear');
     const iconElement = document.querySelector('.footer');
+    const name = document.createElement('span');
+    name.setAttribute('class', 'my-name');
+    name.innerHTML = 'Dominic Abah Odeh';
+    const nigeria = document.createElement('span');
+    nigeria.setAttribute('class', 'my-country');
+    nigeria.innerHTML = 'Nigeria';
 
-    const date = document.createTextNode('/u00a9');
-    const icon = document.createTextNode(dateNow);
-    dateElement.appendChild(date);
-    iconElement.appendChild(icon);
+    const icon = '\u00a9';
+    const date = getCurrentYear();
+
+    const dateAndIcon = `${icon}${date}`;
+    const myCountry = document.createTextNode(' Nigeria');
+    const author = 'Dominic Abah Odeh';
+    const arrows = ` ⤆${author}⤇`;
+    name.textContent = arrows;
+    iconElement.textContent = dateAndIcon;
+    iconElement.appendChild(name);
+    iconElement.appendChild(myCountry);
+}
+const options = {
+    weekday : 'long',
+    day : 'numeric',
+    month : 'long',
+    year : 'numeric'
+}
+function getModifiedDate() {
+    return new Date(document.lastModified).toLocaleDateString('en-US', options);
+}
+function insertTextAndModifiedDate() {
+    const dateContainer = document.getElementById('lastModified');
+
+    // Create a text node with Last Modified Date
+    const lastModifiedDate = document.createTextNode("Last Modified Date: ");
+
+    // Create a text node with the current date
+    const dateTextNode = document.createTextNode(getModifiedDate());
+
+    // Append the icon and text node to the container element
+    dateContainer.appendChild(lastModifiedDate);
+    dateContainer.appendChild(dateTextNode);
 }
 
 insertDateAndIcon();
+insertTextAndModifiedDate();
